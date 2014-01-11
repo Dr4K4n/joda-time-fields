d7c025f96f63d6bb2be7dc755fec505198d6405b
Stefan Erichsen <stefan.erichsen@prodyna.com>
2014-01-11 21:44:46 +0100

    intial commit
    first development version, works for me, but needs some polishing, a
    proper demo and documentation.

---
+++ joda-time-fields/src/test/java/com/prodyna/DateTimeFieldTest.java
@@ -0,0 +1,13 @@
+package com.prodyna;
+
+import junit.framework.Assert;
+import org.junit.Test;
+
+// JUnit tests here
+public class DateTimeFieldTest {
+
+	@Test
+	public void thisAlwaysPasses() {
+		Assert.assertEquals(true, true);
+	}
+}
